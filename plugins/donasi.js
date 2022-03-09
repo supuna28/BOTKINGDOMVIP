33471e09d19f7568d4bfe720cd8bc236f8ce9bf7
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-09 17:06:48 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,5 @@
 let handler =  m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Telkomsel [081227144597]
-╰────
-
-╭─「 Donasi • Non Pulsa 」
-│ • Gopay [081227144597]
+╭─「🙂
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']

dcd4025c2127bb99557f1517239f4f337ee06c31
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-09 17:10:49 +0530

    Update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, args }) => {
     const fetch = (await import('node-fetch')).default
     let full = /f$/i.test(command)
     let text = args.join` `
-    if (!text) return conn.reply(m.chat, 'Tidak ada teks untuk di cari', m)
+    if (!text) return conn.reply(m.chat, 'සර්ච් කරන්න්', m)
     let url = 'https://google.com/search?q=' + encodeURIComponent(text)
     let search = await googleIt(text)
     let msg = search.articles.map(({

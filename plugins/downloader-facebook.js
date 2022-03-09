1365c6dd7f2c79d961ebc80a7d4dc48258d04f93
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-09 17:07:31 +0530

    Update downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -1,6 +1,6 @@
 import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/xxxxxxx/`
     const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
     for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
 }

e4dded22d15f850f62e1d139589eb2e63afa48f6
supuna <92195424+supuna28@users.noreply.github.com>
2022-03-11 08:37:37 +0530

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
   await conn.sendHydrated(m.chat, `
 📌 *Title:* ${title}
 🔗 *Url:* ${url}
-🖹 *Description:* ${description}
+❤️ *Description:* ${description}
 ⏲️ *Published:* ${publishedTime}
 ⌚ *Duration:* ${durationH}
 👁️ *Views:* ${viewH}
